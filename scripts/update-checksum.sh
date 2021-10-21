dbf5e87258663fa656cd5203f072f23bacc43628
Jörg Thalheim <joerg@thalheim.io>
2021-10-21 07:47:14 +0200

    README: update fetchurl checksum

--- scripts/update-checksum.sh
+++ scripts/update-checksum.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$SCRIPT_DIR/.."
 
-tag=$(git describe)
+tag=$(curl --silent "https://api.github.com/repos/nix-community/nix-direnv/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 sha256=$(direnv fetchurl "https://raw.githubusercontent.com/nix-community/nix-direnv/${tag}/direnvrc" | grep -m1 -o 'sha256-.*')
 
-sed -i README.md -e "s!sha256-[^\"]+!sha256-${sha256}!"
+sed -i README.md -e "s!sha256-.*+!sha256-${sha256}\"!"
 git add README.md
 git commit -m "README: update fetchurl checksum"
 git push
