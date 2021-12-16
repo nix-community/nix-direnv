b61138e1308800d14f5c7a8dd8d7911eead00707
Jörg Thalheim <joerg@thalheim.io>
2021-12-16 10:14:43 +0100

    scripts/update-checksums.sh: also update template

--- scripts/update-checksum.sh
+++ scripts/update-checksum.sh
@@ -7,7 +7,7 @@ cd "$SCRIPT_DIR/.."
 tag=$(curl --silent "https://api.github.com/repos/nix-community/nix-direnv/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 sha256=$(direnv fetchurl "https://raw.githubusercontent.com/nix-community/nix-direnv/${tag}/direnvrc" | grep -m1 -o 'sha256-.*')
 
-sed -i README.md -e "s!sha256-.*!sha256-${sha256}\"!"
-git add README.md
-git commit -m "README: update fetchurl checksum"
-git push
+sed -i README.md template/.envrc -e "s!sha256-.*!${sha256}\"!"
+git add README.md template/.envrc
+git commit -m "update fetchurl checksum"
+#git push
