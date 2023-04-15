87c354518cbe9c10506d5564215c7391240d57f0
Jörg Thalheim <joerg@thalheim.io>
2023-04-15 17:44:58 +0200

    script/create-release.sh: better flow

--- scripts/update-checksum.sh
+++ scripts/update-checksum.sh
@@ -4,10 +4,11 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$SCRIPT_DIR/.."
 
-tag=$(curl --silent "https://api.github.com/repos/nix-community/nix-direnv/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+tag=$(git describe --tags | cut -d- -f1)
 sha256=$(direnv fetchurl "https://raw.githubusercontent.com/nix-community/nix-direnv/${tag}/direnvrc" | grep -m1 -o 'sha256-.*')
 
 sed -i README.md templates/flake/.envrc -e "s!sha256-.*!${sha256}\"!"
 git add README.md templates/flake/.envrc
 git commit -m "update fetchurl checksum"
-#git push
+echo "Now run: git push"
+echo "And create a release at https://github.com/nix-community/nix-direnv/releases for version ${tag}"
