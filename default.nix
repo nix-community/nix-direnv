c3c23453e4f1b39196227a89ed4029a8791b1a9d
Arthur Noel <arthur@ischium.net>
2023-11-28 04:40:48 +0000

    refactor flake, shell, lint, and test entry
    * fix deprecation warnings
    * define dependencies once
    * expose all supported nix versions in nixpkgs for test
    * use ruff for both check and format, dropping black

--- default.nix
+++ default.nix
@@ -1,9 +1,12 @@
-{ stdenv, nix, gnugrep, lib }:
+{ stdenv, lib, nix, gnugrep, runCommand, shellcheck, direnv, ruff, mypy, python3 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation (finalAttrs: {
   name = "nix-direnv";
 
-  src = ./.;
+  src = builtins.path {
+    inherit (finalAttrs) name;
+    path = ./.;
+  };
 
   postPatch = ''
     sed -i "2iNIX_BIN_PREFIX=${nix}/bin/" direnvrc
@@ -13,10 +16,41 @@ stdenv.mkDerivation {
     install -m400 -D direnvrc $out/share/nix-direnv/direnvrc
   '';
 
+  passthru.tests = {
+    lint = runCommand "lint-${finalAttrs.name}" {
+      nativeBuildInputs = [shellcheck ruff mypy];
+    } ''
+      set -eu
+      cd ${finalAttrs.src}
+      export RUFF_CACHE_DIR=$TMP/ruff
+      echo run shellcheck
+      shellcheck direnvrc
+      echo run ruff format
+      ruff format --check tests
+      echo run ruff check
+      ruff check tests
+      echo run mypy
+      mypy tests
+      touch $out
+    '';
+    test = finalAttrs.overrideAttrs {
+      name = "test-${finalAttrs.name}-${nix.name}";
+      nativeBuildInputs = [
+        direnv
+        nix
+      ] ++ (with python3.pkgs; [
+        pytestCheckHook
+        pytest-cov
+        pytest-randomly
+        pytest-sugar
+      ]);
+    };
+  };
+
   meta = with lib; {
     description = "A fast, persistent use_nix implementation for direnv";
     homepage = "https://github.com/nix-community/nix-direnv";
     license = licenses.mit;
     platforms = platforms.unix;
   };
-}
+})
--- flake.nix
+++ flake.nix
@@ -1,42 +1,67 @@
 {
   description = "A faster, persistent implementation of `direnv`'s `use_nix`, to replace the built-in one.";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  nixConfig.extra-substituters = [ "https://cache.garnix.io" ];
-  nixConfig.extra-trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  nixConfig = {
+    extra-substituters = ["https://cache.garnix.io"];
+    extra-trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
+  };
+
+  outputs = {self, nixpkgs, flake-utils}:
     flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          packages = {
-            default = pkgs.callPackage ./default.nix { };
-            test-runner-stable = pkgs.callPackage ./test-runner.nix {
-              nixVersion = "stable";
-            };
-            test-runner-unstable = pkgs.callPackage ./test-runner.nix { 
-              nixVersion = "unstable";
-            };
-          };
-          devShells.default = pkgs.callPackage ./shell.nix { };
-          apps.test-runner = {
-            type = "app";
-            program = "${self.packages.${system}.test-runner}";
-          };
-          checks = {
-            lint = pkgs.callPackage ./lint.nix { };
-          };
-        }) // {
-      overlay = final: prev: {
-        nix-direnv = final.callPackage ./default.nix { };
+    (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (pkgs) lib;
+    in rec {
+
+      packages.default = pkgs.callPackage ./default.nix {};
+
+      devShells.default = pkgs.mkShellNoCC {
+        inputsFrom = [ self.checks.${system}.lint self.checks.${system}.test ];
+        nativeBuildInputs = with pkgs; [
+          gnumake
+          jq
+        ];
       };
+
+      checks =
+        packages.default.passthru.tests
+        // lib.mapAttrs' (name: test: lib.nameValuePair ("test_" + name) test) (
+          lib.genAttrs (
+            builtins.filter (
+              version:
+              let
+                pkg = pkgs.nixVersions.${version};
+              in
+                (builtins.tryEval pkg).success
+                && builtins.isAttrs pkg
+                && pkg ? version
+                && builtins.compareVersions pkg.version "2.4" >= 0
+            )
+            (builtins.attrNames pkgs.nixVersions)
+          ) (version:
+            (pkgs.callPackage ./default.nix {
+              nix = pkgs.nixVersions.${version};
+            })
+            .tests
+            .test)
+        );
+
+    })
+    // {
+
+      overlays.default = final: _prev: {
+        nix-direnv = final.callPackage ./default.nix {};
+      };
+
       templates.default = {
         path = ./templates/flake;
-        description = "nix flake new -t github:Mic92/nix-direnv .";
+        description = "nix flake new -t github:nix-community/nix-direnv .";
       };
+
     };
 }
