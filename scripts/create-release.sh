04b2367f018c6cd9a94668a4585e55910d5e5330
Arthur Noel <arthur@ischium.net>
2023-12-03 08:47:20 +0000

    simplify required version checks using `sort --version-sort`

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -10,16 +10,13 @@ if [[ -z "$version" ]]; then
     echo "USAGE: $0 version" 2>/dev/null
     exit 1
 fi
-[[ $version =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)(-?)([^+]*)(\+?)(.*)$ ]]
-declare -a ver; ver=("${BASH_REMATCH[@]:1}")
 
 if [[ "$(git symbolic-ref --short HEAD)" != "master" ]]; then
     echo "must be on master branch" 2>/dev/null
     exit 1
 fi
 
-sed -i direnvrc \
-    -e 's!\(declare major=\).*\( # UPDATE(nix-direnv version)\)!\1'"${ver[0]@Q} minor=${ver[1]@Q} patch=${ver[2]@Q}"'\2!'
+sed -Ei "s!(NIX_DIRENV_VERSION=).*!\1$version!" direnvrc
 
 sed -i README.md templates/flake/.envrc \
     -e 's!\(nix-direnv/\).*\(/direnvrc\)!\1'"${version}"'\2!' \
