5752ec0958610009f4b8d84da5c60fdf661ff635
Jörg Thalheim <joerg@thalheim.io>
2023-12-03 10:12:31 +0100

    fix template after treefmt
    fixes https://github.com/nix-community/nix-direnv/issues/431
    
    It's probably for the best if it's also deadnix-safe for other users

--- templates/flake/flake.nix
+++ templates/flake/flake.nix
@@ -3,7 +3,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { nixpkgs, flake-utils }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
