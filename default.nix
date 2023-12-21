f9a08ca02596d9f23a741593a832b79791f24033
Jörg Thalheim <joerg@thalheim.io>
2023-12-21 09:22:40 +0100

    nix-direnv: also bump version in default.nix

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@
 # https://github.com/abathur/resholve/issues/107
 resholve.mkDerivation rec {
   pname = "nix-direnv";
-  version = "3.0.1";
+  version = "3.0.2";
 
   src = builtins.path {
     path = ./.;
