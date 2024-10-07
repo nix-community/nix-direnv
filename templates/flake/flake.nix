2154fdbb8331a59c90564e0049d454b6187646f7
Peter A. <ink.splatters@pm.me>
2024-10-07 08:18:01 +0200

    Added `nix-systems` input and made `flake-utils.inputs.systems` follow it.

--- templates/flake/flake.nix
+++ templates/flake/flake.nix
@@ -1,7 +1,11 @@
 {
   description = "A basic flake with a shell";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.systems.url = "github.com:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
 
   outputs =
     { nixpkgs, flake-utils, ... }:
