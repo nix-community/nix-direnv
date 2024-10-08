338043c4e8d4f798fd5269743433e4d3d72229b6
ginkogruen <93037574+ginkogruen@users.noreply.github.com>
2024-10-08 08:06:07 +0200

    Fix syntax for nix-systems input
    Removed unsupported .com in nix-systems url input.

--- templates/flake/flake.nix
+++ templates/flake/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A basic flake with a shell";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.systems.url = "github.com:nix-systems/default";
+  inputs.systems.url = "github:nix-systems/default";
   inputs.flake-utils = {
     url = "github:numtide/flake-utils";
     inputs.systems.follows = "systems";
