73293798d536482d899817d6e8289ea8e8acffea
Jörg Thalheim <joerg@thalheim.io>
2026-02-08 21:40:05 +0100

    bump version 3.1.1

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@
 # https://github.com/abathur/resholve/issues/107
 resholve.mkDerivation rec {
   pname = "nix-direnv";
-  version = "3.1.0";
+  version = "3.1.1";
 
   src = builtins.path {
     path = ./.;
