ac94bdde2366a356cd911da19b66cc34abeda53e
Jörg Thalheim <joerg@thalheim.io>
2026-02-08 21:41:47 +0100

    treefmt: drop redundant nixfmt-rfc-style package override
    nixfmt-rfc-style has been merged into pkgs.nixfmt, which is already
    the default package used by treefmt-nix's nixfmt program.

--- treefmt.nix
+++ treefmt.nix
@@ -2,36 +2,33 @@
 {
   imports = [ inputs.treefmt-nix.flakeModule ];
 
-  perSystem =
-    { pkgs, ... }:
-    {
-      treefmt = {
-        # Used to find the project root
-        projectRootFile = ".git/config";
+  perSystem = _: {
+    treefmt = {
+      # Used to find the project root
+      projectRootFile = ".git/config";
 
-        programs = {
-          deadnix.enable = true;
-          deno.enable = true;
-          nixfmt.enable = true;
-          nixfmt.package = pkgs.nixfmt-rfc-style;
-          shellcheck.enable = true;
-          shfmt.enable = true;
-          statix.enable = true;
-          yamlfmt.enable = true;
-        };
+      programs = {
+        deadnix.enable = true;
+        deno.enable = true;
+        nixfmt.enable = true;
+        shellcheck.enable = true;
+        shfmt.enable = true;
+        statix.enable = true;
+        yamlfmt.enable = true;
+      };
 
-        settings.formatter = {
-          shellcheck.includes = [
-            "direnvrc"
-            "tests/*.bash"
-            "tests/*.bats"
-          ];
-          shfmt.includes = [
-            "direnvrc"
-            "tests/*.bash"
-            "tests/*.bats"
-          ];
-        };
+      settings.formatter = {
+        shellcheck.includes = [
+          "direnvrc"
+          "tests/*.bash"
+          "tests/*.bats"
+        ];
+        shfmt.includes = [
+          "direnvrc"
+          "tests/*.bash"
+          "tests/*.bats"
+        ];
       };
     };
+  };
 }
